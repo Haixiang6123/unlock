e471f105ae4be04387aa9fbde5e329bccd77c7bf
Haixiang6123 <haixiang6123@gmail.com>
2019-09-19 13:41:24 -0700

    Update dist folder

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const puppeteer = require('puppeteer');
 
       await page.goto(links[i]);
 
-      await page.screenshot({path: `unlock-${i}.png`, fullPage: true});
+      await page.screenshot({path: `./dist/unlock-${i}.png`, fullPage: true});
     }
 
     await browser.close();
